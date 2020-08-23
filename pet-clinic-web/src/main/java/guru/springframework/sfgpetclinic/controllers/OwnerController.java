7519ec5aac0081a45d70ed31ddfe2476b50360cf
ihor.lapka <ihor.lapka@everymatrix.com>
2020-08-23 12:54:00 +0300

    added MockMvc Tests for Owner Controller. closes #51

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("owners")
+@RequestMapping("/owners")
 @Controller
 public class OwnerController {
     
