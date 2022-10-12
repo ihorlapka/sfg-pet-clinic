a7264385769dbbd7f2848c629401609bc88b325b
ihorlapka <ihor.lapka@everymatrix.com>
2022-10-12 14:32:32 +0300

    added comment after start
    added another comment

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,10 +1,12 @@
 package guru.springframework.sfgpetclinic;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@Slf4j
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 class SfgPetClinicApplicationTests {
@@ -12,6 +14,8 @@ class SfgPetClinicApplicationTests {
     @Test
     void contextLoads() {
 
+        log.info("some comment1");
+        log.info("some comment2");
     }
 
 }
